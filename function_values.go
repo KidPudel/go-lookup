e6155724e1fac936aef2e8b839f54b5c52dec3db
Iggy Kuperson <63263301+KidPudel@users.noreply.github.com>
2024-01-09 00:35:22 +0300

    Create function_values.go

---
+++ function_values.go
@@ -0,0 +1,16 @@
+func compute(fn func(float64, float64)) float64 {
+  return fn(3, 4)
+}
+
+func main() {
+  hypot := func(x, y float64) float64 {
+    return math.Sqrt(x*x + xy*y)
+  }
+
+  fmt.Println(hypot(5, 12)) // 13
+
+  fmt.Println(compute(hypot)) // 5
+
+  fmt.Println(compute(math.Pow)) // 81
+   
+}
